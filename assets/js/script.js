80c63a16871c431b4a05f6c878931a9c35e645ad
Ibrahim Abu Seada <64257417+abuseada@users.noreply.github.com>
2026-06-10 17:13:08 +0000

    FAQs Style And Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,26 @@ servCard.forEach(e => {
         }
     });
 });
+
+const faqCard = document.querySelectorAll('.accordion');
+
+faqCard.forEach(e => {
+    const header = e.querySelector('.faqhead');
+    const body = e.querySelector('.faqbody');
+
+    header.addEventListener('click', j => {
+        const isOpen = e.classList.contains('active');
+
+        // collapse all
+        faqCard.forEach(i => {
+            i.classList.remove('active');
+            i.querySelector('.faqbody').style.maxHeight = null;
+        });
+
+        // ecpand clicked
+        if(!isOpen) {
+            e.classList.add('active');
+            body.style.maxHeight = ContentVisibilityAutoStateChangeEvent.scrollHeight + "px";
+        }
+    });
+});
